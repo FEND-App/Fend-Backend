2c3287912d119fb1e5263d6adc948c344209cf13
GASC1211 <48170476+Gasc1211@users.noreply.github.com>
2025-04-22 16:31:47 -0600

    Change .env path to project root

--- src/database.py
+++ src/database.py
@@ -5,18 +5,16 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 
-load_dotenv("../secrets/.env")
-
+load_dotenv()
 DB_URL = os.getenv("DB_URL")
+ASYNC_DATABASE_URL = os.getenv("DB_URL_ASYNC")
 
 engine = create_engine(DB_URL, pool_size=20, max_overflow=0)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Motor asíncrono
-ASYNC_DATABASE_URL = os.getenv("DB_URL_ASYNC")
 async_engine = create_async_engine(ASYNC_DATABASE_URL)
 AsyncSessionLocal = sessionmaker(
     bind=async_engine, class_=AsyncSession, expire_on_commit=False)
-
 
 Base = declarative_base()
